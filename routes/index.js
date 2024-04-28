0d55ee4236f5c09c4b2a16983d590e7b9409f051
zeroboycoder <pyaesonekhant@tumawlamyine.edu.mm>
2024-04-28 19:19:02 +0700

    Add hello world route

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const route = require("express").Router();
 const adminRoute = require("./admin");
 const agentRoute = require("./agent");
 
+route.use("/", (req, res) => res.send("Hello World!"));
 route.use("/admin", adminRoute);
 route.use("/agent", agentRoute);
 
